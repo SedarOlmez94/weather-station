1289966c33d162dc7587bd3f0f0e63e1fbbb0a1d
SedarOlmez94 <olmez49@gmail.com>
2025-03-07 20:49:58 +0000

    ✨ created the WeatherApiAccess object.

--- src/code/weatherApiAccess.py
+++ src/code/weatherApiAccess.py
@@ -4,3 +4,16 @@ Email: olmez49@gmail.com
 Description...
 '''
 
+import requests
+import json
+
+
+class WeatherApiAccess:
+    def __init__(self, url: str):
+        self.url = url
+
+
+    def getWeatherStations(self):
+        url = 'https://api.weather.gov/stations'
+        response = requests.get(url)
+        return response.json()
